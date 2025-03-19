c46dd2be2db1b404d35dc4d183ee4624b54d8a99
nafisaa11 <jnafisah50@gmail.com>
2025-03-19 09:05:44 +0700

    final

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Halo! Saya Nafisah</h1>
         <p className={styles.description}>
-        Saya mahasiswa Teknik Informatika di PENS, dengan minat dalam pengembangan web khususnya backend. Saya juga memiliki pengalaman dalam pengembangan aplikasi mobile dengan Flutter.
+        Saya mahasiswa Teknik Informatika di PENS, dengan minat dalam pengembangan website. Saya juga memiliki pengalaman dalam pengembangan aplikasi mobile dengan Flutter.
         </p>
         <a href="/CV.pdf" download="CV_Jauharotun_Nafisah.pdf" className={styles.contactBtn}>
         <FaDownload /> Lihat CV Saya

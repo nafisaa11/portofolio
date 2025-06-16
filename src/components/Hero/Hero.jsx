2fc846b04eaedff1fbb3121e2adff816c3ff18d9
nafisaa11 <jnafisah50@gmail.com>
2025-06-16 13:26:25 +0700

    merubah hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,11 +9,8 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Halo! Saya Nafisah</h1>
         <p className={styles.description}>
-        Saya mahasiswa Teknik Informatika di PENS, dengan minat dalam pengembangan website. Saya juga memiliki pengalaman dalam pengembangan aplikasi mobile dengan Flutter.
+        Full-Stack Developer (Web & Mobile) dengan keahlian frontend dan backend, berpengalaman dalam integrasi API microservices dan pengembangan sistem mobile yang scalable.
         </p>
-        <a href="/CV.pdf" download="CV_Jauharotun_Nafisah.pdf" className={styles.contactBtn}>
-        <FaDownload /> Lihat CV Saya
-</a>
 
       </div>
       <img
